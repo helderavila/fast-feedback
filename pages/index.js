1be3332b7a5d7a2cfa2bdf0af23e90f65af486dc
Helder Avila <helderavila2016@gmail.com>
2022-06-04 13:06:28 -0300

    Add firebase auth with github (#2)
    Co-authored-by: Helder Avila <helder.avila@telavita.com.br>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useAuth } from '../lib/auth'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const auth = useAuth()
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,39 +19,18 @@ export default function Home() {
          Fast feedback
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <button onClick={() => auth.signinWithGitHub()}>
+          Sign In
+        </button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        {auth?.user && (
+          <button onClick={() => auth.signout()}>
+            Sign Out
+          </button>
+        )}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          {auth?.user?.email}
         </div>
       </main>
 
