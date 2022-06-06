fb63be2be6829b359c0bb46d5c5f28db585f9249
Helder Avila <helder.avila@telavita.com.br>
2022-06-06 19:47:53 -0300

    Chakra UI setup, firestore setup, css reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,43 @@
-import { AuthProvider } from '../lib/auth'
-import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { AuthProvider } from '@/lib/auth'
+
+import theme from '@/styles/theme'
+import Head from 'next/head';
+import { css, Global } from '@emotion/react';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   )
 }
 
